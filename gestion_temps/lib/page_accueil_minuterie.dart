71a27c67e22a15e48d9760d894c283bd2b3cfadc
Louis Falce <louis.falce@hotmail.fr>
2022-05-24 00:30:36 +0200

    buttons et timet ajoutés

--- gestion_temps/lib/page_accueil_minuterie.dart
+++ gestion_temps/lib/page_accueil_minuterie.dart
@@ -3,63 +3,77 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_temps/main.dart';
 import 'widgets.dart';
-//import 'package:percent_indicator/percent_indicator.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class PageAccueilMinuterie extends StatelessWidget {
   const PageAccueilMinuterie({Key? key}) : super(key: key);
+  int aRemplacer() {
+    return 1;
+  }
 
-  void methodevide() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Ma gestion du temps')),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
-                Expanded(
-                    child: BoutonGenerique(
-                        couleur: Colors.red,
-                        texte: 'Travail',
-                        taille: 60.0,
-                        action: methodevide)),
-                Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
-                Expanded(
-                    child: BoutonGenerique(
-                        couleur: Colors.green,
-                        texte: 'Pause',
-                        taille: 60.0,
-                        action: methodevide)),
-                Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
-                Expanded(
-                    child: BoutonGenerique(
-                        couleur: Colors.blue,
-                        texte: 'Maxi Pause',
-                        taille: 60.0,
-                        action: methodevide)),
-                Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
-              ],
+        appBar: AppBar(title: Text('Ma gestion du temps')),
+        body: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints contraints) {
+          final double largeurDisponible = contraints.maxWidth;
+          return Column(children: [
+            Row(children: [
+              Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
+              Expanded(
+                  child: BoutonGenerique(
+                      couleur: Colors.red,
+                      texte: 'Travail',
+                      taille: 45,
+                      action: aRemplacer)),
+              Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
+              Expanded(
+                  child: BoutonGenerique(
+                      couleur: Colors.green,
+                      texte: 'Mini pause',
+                      taille: 45,
+                      action: aRemplacer)),
+              Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
+              Expanded(
+                  child: BoutonGenerique(
+                      couleur: Colors.greenAccent,
+                      texte: 'Maxi pause',
+                      taille: 45,
+                      action: aRemplacer)),
+              Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT))
+            ]),
+            Expanded(
+              child: CircularPercentIndicator(
+                radius: largeurDisponible / 3,
+                lineWidth: 10.0,
+                percent: 1,
+                center: Text(
+                  '30:00',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                progressColor: Colors.deepPurpleAccent,
+              ),
             ),
-          ],
-        ),
+          ]);
+        }),
         bottomNavigationBar: Row(
           children: [
             Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
             Expanded(
                 child: BoutonGenerique(
-                    couleur: Colors.blue,
-                    texte: 'Arreter',
-                    taille: 60.0,
-                    action: methodevide)),
+                    couleur: Colors.teal,
+                    texte: 'Travail',
+                    taille: 45,
+                    action: aRemplacer)),
             Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT)),
             Expanded(
                 child: BoutonGenerique(
-                    couleur: Colors.blue,
-                    texte: 'Relancer',
-                    taille: 60.0,
-                    action: methodevide)),
-            Padding(padding: EdgeInsets.all(16.0))
+                    couleur: Colors.grey,
+                    texte: 'Mini pause',
+                    taille: 45,
+                    action: aRemplacer)),
+            Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT))
           ],
         ));
   }
