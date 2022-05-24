d3c2192444a3083f8586b1668328cc2870259a73
Louis Falce <louis.falce@hotmail.fr>
2022-05-24 12:32:48 +0200

    minuteur fonctionnel

--- gestion_temps/lib/page_accueil_minuterie.dart
+++ gestion_temps/lib/page_accueil_minuterie.dart
@@ -4,15 +4,19 @@ import 'package:flutter/material.dart';
 import 'package:gestion_temps/main.dart';
 import 'widgets.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'minuteur.dart';
 
 class PageAccueilMinuterie extends StatelessWidget {
   const PageAccueilMinuterie({Key? key}) : super(key: key);
+
   int aRemplacer() {
     return 1;
   }
 
   @override
   Widget build(BuildContext context) {
+    Minuteur minuteur = new Minuteur();
+    minuteur.demarrerTravail();
     return Scaffold(
         appBar: AppBar(title: Text('Ma gestion du temps')),
         body: LayoutBuilder(
@@ -44,16 +48,26 @@ class PageAccueilMinuterie extends StatelessWidget {
               Padding(padding: EdgeInsets.all(REMPLISSAGE_DEFAUT))
             ]),
             Expanded(
-              child: CircularPercentIndicator(
-                radius: largeurDisponible / 3,
-                lineWidth: 10.0,
-                percent: 1,
-                center: Text(
-                  '30:00',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                progressColor: Colors.deepPurpleAccent,
-              ),
+              child: StreamBuilder(
+                  initialData: ModeleMinuteur('00:00', 1),
+                  stream: minuteur.stream(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    ModeleMinuteur minuteur = snapshot.data;
+                    return Container(
+                        child: CircularPercentIndicator(
+                      radius: largeurDisponible / 4,
+                      lineWidth: 10.0,
+                      // ignore: unnecessary_null_comparison
+                      percent: (minuteur.pourcentage == null)
+                          ? 1
+                          : minuteur.pourcentage,
+                      center: Text(
+                        (minuteur.temps == null) ? '00:00' : minuteur.temps,
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      progressColor: Colors.green,
+                    ));
+                  }),
             ),
           ]);
         }),
